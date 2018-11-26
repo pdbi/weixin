833ad82ea7bbf7cc3c7c3d18e5eb275fada6522c
pdbi <peidong@trinitesolutions.com>
2018-11-26 20:42:36 +0800

    test

--- weixin-java-mp-demo-springboot/src/main/java/com/github/binarywang/demo/wx/mp/WxMpDemoApplication.java
+++ weixin-java-mp-demo-springboot/src/main/java/com/github/binarywang/demo/wx/mp/WxMpDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WxMpDemoApplication {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(WxMpDemoApplication.class, args);
     }
